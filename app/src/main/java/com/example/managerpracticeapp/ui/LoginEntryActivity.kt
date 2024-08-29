50ffde933ee9b0f6fce773a99e21b0c5479442dd
changukLee <ckddnr5527@gmail.com>
2024-08-29 18:28:19 +0900

    design: 로그인 진입 화면 디자인 추가

---
+++ app/src/main/java/com/example/managerpracticeapp/ui/LoginEntryActivity.kt
@@ -0,0 +1,15 @@
+package com.example.managerpracticeapp.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.managerpracticeapp.R
+
+class LoginEntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_entry)
+    }
+}
