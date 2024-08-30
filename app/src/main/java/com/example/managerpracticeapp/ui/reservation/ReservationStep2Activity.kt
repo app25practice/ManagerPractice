2597039370edf063790324c075cab8dab34bbeed
changukLee <ckddnr5527@gmail.com>
2024-08-30 22:35:02 +0900

    feat: 예약 step2 구현

---
+++ app/src/main/java/com/example/managerpracticeapp/ui/reservation/ReservationStep2Activity.kt
@@ -0,0 +1,46 @@
+package com.example.managerpracticeapp.ui.reservation
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.managerpracticeapp.R
+import com.example.managerpracticeapp.databinding.ActivityReservationStep2Binding
+
+class ReservationStep2Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityReservationStep2Binding
+    private var relation = "본인"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityReservationStep2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setRelationDropDown()
+        navigateToPrevious()
+    }
+
+    private fun setRelationDropDown() {
+        val relationOptions = resources.getStringArray(R.array.relation_option)
+
+        val arrayAdapter = ArrayAdapter(this, R.layout.item_dropdown, relationOptions)
+        binding.relationAutoCompleteTextView.setAdapter(arrayAdapter)
+
+        binding.relationAutoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
+            relation = parent.getItemAtPosition(position).toString()
+            Toast.makeText(this, "선택된 값: $relation", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navigateToPrevious() {
+        binding.backBtn.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.previousBtn.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+}
