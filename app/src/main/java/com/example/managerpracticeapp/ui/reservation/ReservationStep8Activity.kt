95709f53b092741cf0231988207c9cfcaa70b84e
SHEEN <normengdie@pusan.ac.kr>
2024-09-03 00:50:00 +0900

    feat : activity_reservation_step8 UI

---
+++ app/src/main/java/com/example/managerpracticeapp/ui/reservation/ReservationStep8Activity.kt
@@ -0,0 +1,16 @@
+package com.example.managerpracticeapp.ui.reservation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.managerpracticeapp.databinding.ActivityReservationStep8Binding
+
+class ReservationStep8Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityReservationStep8Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityReservationStep8Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+    }
