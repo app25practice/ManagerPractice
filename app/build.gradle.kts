cb13c99e24b4a7e47a76fe1ad400dabfd4d99a75
Jeong hoon <hoondb@naver.com>
2024-09-03 17:40:34 +0900

    chore: 카카오 SDK 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.kakao.sdk.all)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
