309ad8fdd7771741dc4271940f33fbe6895ff881
SHEEN <normengdie@pusan.ac.kr>
2024-09-03 19:32:34 +0900

    feat : RecyclerView Manger Adapter

--- app/src/main/java/com/example/managerpracticeapp/ui/reservation/ReservationStep8Activity.kt
+++ app/src/main/java/com/example/managerpracticeapp/ui/reservation/ReservationStep8Activity.kt
@@ -2,7 +2,9 @@ package com.example.managerpracticeapp.ui.reservation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.managerpracticeapp.Manager
 import com.example.managerpracticeapp.databinding.ActivityReservationStep8Binding
+import com.example.managerpracticeapp.ui.adapter.RecyclerViewMangerAdapter
 
 class ReservationStep8Activity : AppCompatActivity() {
     private lateinit var binding: ActivityReservationStep8Binding
@@ -11,6 +13,17 @@ class ReservationStep8Activity : AppCompatActivity() {
         binding = ActivityReservationStep8Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        createRecyclerView()
 
+
+    }
+    private fun createRecyclerView(){
+        val managerList : ArrayList<Manager> = getListFromDb()
+        val adapter = RecyclerViewMangerAdapter(managerList)
+
+    }
+    private fun getListFromDb(): ArrayList<Manager> {
+        val managerList : ArrayList<Manager> = ()  // DB에서 데이터 받아옴
+        return managerList
     }
     }
