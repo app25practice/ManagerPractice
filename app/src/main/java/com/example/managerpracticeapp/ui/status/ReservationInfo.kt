45f80f1c89981eddb1a30134e93723123fcb1ef9
Jeong hoon <hoondb@naver.com>
2024-09-05 13:00:38 +0900

    feat: 예약 취소 화면 기능 구현

---
+++ app/src/main/java/com/example/managerpracticeapp/ui/status/ReservationInfo.kt
@@ -0,0 +1,12 @@
+package com.example.managerpracticeapp.ui.status
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.Date
+
+@Parcelize
+data class ReservationInfo(
+    val id: String,
+    val name: String,
+    val date: Date
+) : Parcelable
